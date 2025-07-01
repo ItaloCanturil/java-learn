7cfc0c347faf5a9f4df4c3a470b2edf34e75688c
ItaloCanturil <italo.cantur@gmail.com>
2025-07-01 08:17:59 -0300

    exercio core

---
+++ src/academy/devdojo/maratonajava/javacore/Bintroducaometodos/dominio/Funcionario.java
@@ -0,0 +1,33 @@
+package academy.devdojo.maratonajava.javacore.Bintroducaometodos.dominio;
+
+public class Funcionario {
+    public String nome;
+    public int idade;
+    public double[] salarios;
+
+    public void imprimeDados() {
+        System.out.println("-----------");
+        System.out.println(this.nome);
+        System.out.println(this.idade);
+
+        if (salarios == null) {
+            return;
+        }
+        for (double salario : this.salarios) {
+            System.out.print(salario + " ");
+        }
+    }
+
+    public void media() {
+        if (salarios != null) {
+            return;
+        }
+        double media = 0;
+        for (double salario : this.salarios) {
+            media += salario;
+        }
+
+        media /= this.salarios.length;
+        System.out.println("\nMédia salarial: " + media);
+    }
+}
